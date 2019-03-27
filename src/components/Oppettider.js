db0f43bfa89e5296a7aeab5a7548404166b81c43
Niklas Albinsson <albinssonniklas@gmail.com>
2019-03-28 00:52:39 +0100

    Added query to grab data from CMS

--- src/components/Oppettider.js
+++ src/components/Oppettider.js
@@ -1,22 +1,47 @@
 import React from "react"
+import { StaticQuery, graphql } from "gatsby"
 import { OppettiderStyles } from "../styles/Index"
 
 const Oppettider = () => (
-  <OppettiderStyles id="oppettider">
-    <h2>Öppettider:</h2>
-    <div>
-      <p>Måndag - Fredag:</p>
-      <p>10.00 - 18.00</p>
-    </div>
-    <div>
-      <p>Lördag:</p>
-      <p>10.00 - 14.00</p>
-    </div>
-    <div>
-      <p>Söndag:</p>
-      <p>Helgvila</p>
-    </div>
-  </OppettiderStyles>
+  <StaticQuery
+    query={graphql`
+      query {
+        allFile(filter: { sourceInstanceName: { eq: "oppettider" } }) {
+          edges {
+            node {
+              childMarkdownRemark {
+                frontmatter {
+                  mon_fri
+                  sat
+                  sun
+                }
+              }
+            }
+          }
+        }
+      }
+    `}
+    render={data => {
+      const hours = data.allFile.edges[0].node.childMarkdownRemark.frontmatter
+      return (
+        <OppettiderStyles id="oppettider">
+          <h2>Öppettider:</h2>
+          <div>
+            <p>Måndag - Fredag:</p>
+            <p>{hours.mon_fri}</p>
+          </div>
+          <div>
+            <p>Lördag:</p>
+            <p>{hours.sat}</p>
+          </div>
+          <div>
+            <p>Söndag:</p>
+            <p>{hours.sun}</p>
+          </div>
+        </OppettiderStyles>
+      )
+    }}
+  />
 )
 
 export default Oppettider
